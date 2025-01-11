8458d203ebb3e9231b3bea6dc7b937f60ea256b7
jake-47 <ijac@tutanota.com>
2025-01-11 12:37:11 +0530

    Updated gh-pages 10e8659

--- toc.js
+++ toc.js
@@ -8,7 +8,7 @@ class MDBookSidebarScrollbox extends HTMLElement {
         super();
     }
     connectedCallback() {
-        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="introduction.html">Introduction</a></li><li class="chapter-item expanded affix "><li class="spacer"></li><li class="chapter-item expanded "><a href="chapter_1.html">Chapter 1</a></li></ol>';
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="homepage.html">Homepage</a></li><li class="chapter-item expanded "><a href="borg-guide.html">Borg Simple Guide for MacOS</a></li><li class="chapter-item expanded "><a href="bitcoin-letter.html">Bitcoin Letter</a></li></ol>';
         // Set the current, active page, and reveal it if it's hidden
         let current_page = document.location.href.toString();
         if (current_page.endsWith("/")) {
